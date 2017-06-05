dda922c8f9a3fb2f2c0984589e815348510a77d8
lpaleks <lpaleks@mail.ru>
2017-06-05 21:39:22 +0300

    nine commit

--- register.php
+++ register.php
@@ -4,38 +4,74 @@ $title="О котах";
 $desc="Нужна ли кошка в квартире?";
 $keywords="Нужна ли кошка в квартире?";
   require_once('templates/top.php');
-?>
+
+if($_POST){
+	$arr = [];
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+	$password2 = $_POST['password2'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$date = $_POST['date'];
+
+	if($password == $password2){
+		echo "<h3>Пароли верны</h3>";
+
+	} else {
+		$arr[] = "Не совпадают пароли"; 
+	
+	}
+	
+}  
+foreach($arr as $arr_err){
+	echo "<p style='color:red; font-size:20px' class='error'>".$arr_err."</p>";
+}
+  /*
+echo "<pre>";
+print_r ($_POST); 
+echo "</pre>";*/
+ ?>
+
+
 	
 	<div class="container">		
 		<div class="col-md-8">
-	<form method="POST" action="index.php">
+	<form method="POST" action="register.php">
 
-  <div class="form-group">
-    <label for="exampleInputEmail1">Введите логин</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="Введите Email">
-  </div>
+	<br>
+    <div class="form-group">
+    <label for="exampleInputLogin">Введите ваш логин</label>
+    <input  class="form-control" id="exampleInputLogin" name="login" placeholder="Jonh Smith" required>
+	</div>
+	
   <br>
   <div class="form-group">
     <label for="exampleInputPassword1">Пароль</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" type="password" name="password" placeholder="Введите пароль">
+    <input type="password" class="form-control" id="exampleInputPassword1" type="password" name="password" placeholder="Введите пароль" required>
+  </div>
+	
+
+	<br>
+    <div class="form-group">
+    <label for="exampleInputPassword2">Повторите пароль</label>
+    <input type="password" class="form-control" id="exampleInputPassword2" type="password" name="password2" placeholder="Повторите пароль" required>
+	</div>
+	
+  <div class="form-group">
+    <label for="exampleInputEmail1">Введите логин</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="Введите Email" required>
   </div>
 	
 	<br>
     <div class="form-group">
-    <label for="exampleInputPassword2">Повторите пароль</label>
-    <input type="password" class="form-control" id="exampleInputPassword2" type="password" name="password_again" placeholder="Повторите пароль">
-	</div>
-
-	<br>
-    <div class="form-group">
     <label for="exampleInputPhone">Введите ваш номер телефона</label>
-    <input  class="form-control" id="exampleInputPhone" type="number" name="phone" placeholder="Введите телефон(+375(..) ........">
+    <input  class="form-control" id="exampleInputPhone" type="number" name="phone" placeholder="Введите телефон(+375(..) ........" required>
 	</div>
 	
 	<br>
     <div class="form-group">
     <label for="exampleInputDate">Дата вашего рождения</label>
-    <input class="form-control" id="exampleInputDate" type="date" name="date" placeholder="">
+    <input class="form-control" id="exampleInputDate" type="date" name="date" placeholder="" required>
 	</div>
 	
   <button type="submit" class="btn btn-default">Регистрация</button>
